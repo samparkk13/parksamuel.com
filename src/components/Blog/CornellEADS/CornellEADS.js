05a7c65dadb3decaaf862f0d8887f7edb09b3eea
“samparkk13” <sam13park@gmail.com>
2024-06-23 02:19:05 -0400

    added youtube link

--- src/components/Blog/CornellEADS/CornellEADS.js
+++ src/components/Blog/CornellEADS/CornellEADS.js
@@ -1,12 +1,25 @@
+import { FaYoutube, FaFilePdf } from 'react-icons/fa';
+
 const CornellEADS = () => {
     return ( 
         <div style={styles.container}>
             <div style={styles.text}>
-                first blog coming soon 
+                First blog coming soon!
                 <br />In the meanwhile, you can read this debrief letter!
             </div>
             <a href="files/DebriefLetter.pdf" target="_blank" rel="noopener noreferrer" style={styles.link}>
+                <FaFilePdf style={{ marginRight: '8px' }} />
                 Debrief Letter Here
+            </a>
+            <br />
+            <br />
+            <div style={styles.text}>
+                Or you can watch this video!
+                <br />
+            </div>
+            <a href="https://www.youtube.com/watch?v=qM4sSyjf8vM&ab_channel=karis" target="_blank" rel="noopener noreferrer" style={styles.link}>
+                <FaYoutube style={{ marginRight: '8px' }} />
+                Video Link Here
             </a>
         </div>
     );
@@ -31,7 +44,9 @@ const styles = {
         fontSize: '16px',
         color: 'blue',
         textDecoration: 'underline',
+        display: 'flex',
+        alignItems: 'center',
     },
 }
- 
+
 export default CornellEADS;
