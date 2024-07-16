8134c7a82c91a92c5b08dfe49aad2ace3d564665
“samparkk13” <sam13park@gmail.com>
2024-07-16 16:24:20 -0400

    blog change

--- src/components/Private/Summer2025Intern.js
+++ src/components/Private/Summer2025Intern.js
@@ -1,4 +1,4 @@
-import { Heading, Box, Flex } from "@chakra-ui/react";
+import { Heading, Box, Flex, Image, List, ListItem } from "@chakra-ui/react";
 
 const Summer2025Intern = () => {
 
@@ -18,6 +18,19 @@ const Summer2025Intern = () => {
                 <Box textAlign="left" fontSize="1.5em" fontWeight="bold">
                     Walmart Global Tech
                 </Box>
+                <Box mx="0%" display="flex" justifyContent="flex-start">
+                    <Image 
+                        src="images/Logos/walmartLogo.jpg"
+                        alt="Walmart Logo"
+                        mt="20px" 
+                        w={{ base: "350px", sm: "30%" }}
+                        minWidth="200px"  
+                        maxW="30%" 
+                        h="auto" 
+                        display="block"
+                        borderRadius= "15px"
+                    />
+                </Box>
                 <br/>
                 <Flex textAlign="left" fontSize="1.0em">
                     <Box fontWeight="bold" mr={6}>
@@ -33,12 +46,12 @@ const Summer2025Intern = () => {
                         Timeline:
                     </Box>
                     <Box fontWeight="medium">
-                        Applied online without referral (05/05) <span>&rarr;</span> 
-                        completed candidate assessment (05/07) <span>&rarr;</span> 
-                        received Karat interview invitation (05/22) <span>&rarr;</span> 
-                        completed first round technical interview via Karat (05/30) <span>&rarr;</span> 
-                        received final round interview invitation (06/04) <span>&rarr;</span> 
-                        completed final round interview with SWE at Walmart (06/05) <span>&rarr;</span> 
+                        Applied online without referral (05/05) <span>&rarr;&nbsp;</span> 
+                        completed candidate assessment (05/07) <span>&rarr;&nbsp;</span> 
+                        received Karat interview invitation (05/22) <span>&rarr;&nbsp;</span> 
+                        completed first round technical interview via Karat (05/30) <span>&rarr;&nbsp;</span> 
+                        received final round interview invitation (06/04) <span>&rarr;&nbsp;</span> 
+                        completed final round interview with SWE at Walmart (06/05) <span>&rarr;&nbsp;</span> 
                         passed interviews but on hiring pause (07/01)
                     </Box>
                 </Flex>
@@ -48,9 +61,12 @@ const Summer2025Intern = () => {
                         Thoughts:
                     </Box>
                     <Box fontWeight="medium">
-                        This was my first interview process for 2025 and the process as a whole wasn't too difficult from a technical POV. 
+                        This was my first interview process for summer 2025 and the process as a whole wasn't too difficult from a technical POV. 
+                        The process definetly gave me a morale boost after not being able to land an internship all throughout sophomore year for the summer. 
                         The Karat interview were LC easy / medium and final round was all behavioral. 
-                        One thing to note: the company overall moves pretty slow, maybe because they were also hiring very early on.
+                        <br/>
+                        One thing to note: the company overall moves pretty slow, maybe because they were also hiring very early on compared to other companies.
+                        My recruiter also had a very slow response time and was out of office multiple times when I tried reaching out. 
                     </Box>
                 </Flex>
             </Box>
@@ -61,6 +77,19 @@ const Summer2025Intern = () => {
             <Box textAlign="left" mt="60px" mx="15%" fontWeight="medium" fontFamily="'Times New Roman', sans-serif" lineHeight="2.0">
                 <Box textAlign="left" fontSize="1.5em" fontWeight="bold">
                     Google
+                </Box>
+                <Box mx="0%" display="flex" justifyContent="flex-start">
+                    <Image 
+                        src="images/Logos/GoogleLogo.jpeg"
+                        alt="Google Logo"
+                        mt="20px" 
+                        w={{ base: "350px", sm: "30%" }}
+                        minWidth="200px"  
+                        maxW="30%" 
+                        h="auto" 
+                        display="block"
+                        borderRadius= "15px"
+                    />
                 </Box>
                 <br/>
                 <Flex textAlign="left" fontSize="1.0em">
@@ -77,7 +106,7 @@ const Summer2025Intern = () => {
                         Timeline:
                     </Box>
                     <Box fontWeight="medium">
-                        Applied online with referral (06/17) <span>&rarr;</span> 
+                        Applied online with referral (06/17) <span>&rarr;&nbsp;</span> 
                     </Box>
                 </Flex>
                 <br/>
@@ -97,6 +126,19 @@ const Summer2025Intern = () => {
                 <Box textAlign="left" fontSize="1.5em" fontWeight="bold">
                     JP Morgan
                 </Box>
+                <Box mx="0%" display="flex" justifyContent="flex-start">
+                    <Image 
+                        src="images/Logos/JPMorganLogo.jpeg"
+                        alt="J.P. Morgan Logo"
+                        mt="20px" 
+                        w={{ base: "350px", sm: "30%" }}
+                        minWidth="200px"  
+                        maxW="30%" 
+                        h="auto" 
+                        display="block"
+                        borderRadius= "15px"
+                    />
+                </Box>
                 <br/>
                 <Flex textAlign="left" fontSize="1.0em">
                     <Box fontWeight="bold" mr={6}>
@@ -112,8 +154,9 @@ const Summer2025Intern = () => {
                         Timeline: 
                     </Box>
                     <Box fontWeight="medium">
-                        Applied online with referral (07/03) <span>&rarr;</span> 
-                        received OA (07/04) <span>&rarr;</span> 
+                        Applied online with referral (07/03) <span>&rarr;&nbsp;</span> 
+                        received OA (07/04) <span>&rarr;&nbsp;</span> 
+                        completed OA - passed 8/15 test cases for first question and 3/15 test cases for second question (07/11) <span>&rarr;&nbsp;</span> 
                     </Box>
                 </Flex>
                 <br/>
@@ -133,6 +176,19 @@ const Summer2025Intern = () => {
                 <Box textAlign="left" fontSize="1.5em" fontWeight="bold">
                     Palantir
                 </Box>
+                <Box mx="0%" display="flex" justifyContent="flex-start">
+                    <Image 
+                        src="images/Logos/PalantirLogo.jpeg"
+                        alt="Palantir Logo"
+                        mt="20px" 
+                        w={{ base: "350px", sm: "30%" }}
+                        minWidth="200px"  
+                        maxW="30%" 
+                        h="auto" 
+                        display="block"
+                        borderRadius= "15px"
+                    />
+                </Box>
                 <br/>
                 <Flex textAlign="left" fontSize="1.0em">
                     <Box fontWeight="bold" mr={6}>
@@ -148,7 +204,9 @@ const Summer2025Intern = () => {
                         Timeline:
                     </Box>
                     <Box fontWeight="medium">
-                        Applied online with referral (07/11) <span>&rarr;</span> 
+                        Received referral (07/08) <span>&rarr;&nbsp;</span> 
+                        Applied online with referral (07/11) <span>&rarr;&nbsp;</span> 
+                        Received OA (07/16) <span>&rarr;&nbsp;</span> 
                     </Box>
                 </Flex>
                 <br/>
@@ -160,6 +218,31 @@ const Summer2025Intern = () => {
                         
                     </Box>
                 </Flex>
+            </Box>
+
+
+            <Box textAlign="left" mt="60px" mx="15%" fontWeight="medium" fontFamily="'Times New Roman', sans-serif" lineHeight="2.0">
+                <Flex textAlign="left" fontSize="1.0em">
+                    <Box fontSize="1.5em" fontWeight="bold" mr={6}>
+                        Other Notable Companies I applied to:
+                    </Box>
+                </Flex>
+                <Flex textAlign="left" fontSize="1.0em">
+                    <Box fontWeight="medium">
+                        <List styleType="disc">
+                            <ListItem>SIG</ListItem>
+                            <ListItem>Apple</ListItem>
+                            <ListItem>Optiver (received OA)</ListItem>
+                            <ListItem>Citadel (completed OA)</ListItem>
+                            <ListItem>DRW</ListItem>
+                            <ListItem>IMC Trading</ListItem>
+                        </List>
+                    </Box>
+                </Flex>
+            </Box>
+
+            <Box textAlign="left" mt="30px" mx="15%" fontWeight="medium" fontFamily="'Times New Roman', sans-serif" lineHeight="2.0">
+                Let me know if you have any questions about any of the above recruitment processes and I will try to answer to my best knowledge and memory!
             </Box>
 
         </Box>
